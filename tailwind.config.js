4a22a4887fa201bbfa4ddc6e8488d80c619055e5
Ben King <btking1990@googlemail.com>
2021-10-02 17:03:55 +0100

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  purge:
-  enabled: true,
-    content: [
-    './src/**/*.html',
-    './src/**/*.vue',
-    './src/**/*.jsx',
-  ],
+  purge: {
+      enabled: false,
+      content: [
+      './src/**/*.html',
+      './src/**/*.vue',
+      './src/**/*.jsx',
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
